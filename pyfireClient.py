a4b8254d5b2de447e21a2efabc6a727374e95689
Ross <rosshamey@gmail.com>
2023-02-18 12:25:58 -0500

    Update pyfireClient.py

--- pyfireClient.py
+++ pyfireClient.py
@@ -34,3 +34,14 @@ def listen_for_messages():
 t = Thread(target=listen_for_messages)
 t.daemon = True
 t.start()
+
+
+while True:
+    to_send =  input()
+    if to_send.lower() == 'q':
+        break
+    date_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S') 
+    to_send = f"{client_color}[{date_now}] {name}{separator_token}{to_send}{Fore.RESET}"
+    s.send(to_send.encode())
+
+s.close()
